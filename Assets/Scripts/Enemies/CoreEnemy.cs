75297eea6aefb7895de408eeb1a043fc436d1707
Isaac Williams <williamsisaac1209@gmail.com>
2022-11-17 00:50:19 +1100

    Added pursuit to enemy States

--- Assets/Scripts/Enemies/CoreEnemy.cs
+++ Assets/Scripts/Enemies/CoreEnemy.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CoreEnemy : Entity
 {
     public int weaponIndex;
+
+    NavMeshAgent nav;
 
     Weapon weapon;
 
     // Start is called before the first frame update
     void Start()
     {
+        nav = GetComponent<NavMeshAgent>();
         currentHealth = Health;
         weapon = new Weapon(Weapons.weaponList[weaponIndex]);
     }
@@ -19,6 +23,7 @@ public class CoreEnemy : Entity
     void Update()
     {
         transform.LookAt(GameManager.player.transform);
+        nav.SetDestination(GameManager.player.transform.position);
         weapon.Update();
         weapon.Fire(transform.position, GameManager.player.transform.position);
     }
